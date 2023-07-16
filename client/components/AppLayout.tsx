e5ec89e4f26414ebf66ba7de1b09096c2eafee77
Min <min.elliot48@gmail.com>
2023-07-16 21:51:30 +1200

    nav bar added

--- client/components/AppLayout.tsx
+++ client/components/AppLayout.tsx
@@ -1,23 +1,52 @@
 import { Link, Outlet } from 'react-router-dom'
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+  MDBIcon,
+} from 'mdb-react-ui-kit'
+import { useState } from 'react'
 
 export default function AppLayout() {
+  const [showNav, setShowNav] = useState(false)
+
   return (
-    <div className="header">
+    <div className="background">
+      <MDBNavbar expand="lg" light bgColor="light">
+        <MDBContainer fluid>
+          <MDBNavbarBrand href="#">RED PING</MDBNavbarBrand>
+          <MDBNavbarToggler
+            type="button"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setShowNav(!showNav)}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showNav}>
+            <MDBNavbarNav>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="home">Home</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Alert List</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="profile" tabIndex={-1}>
+                  Profile
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
       <header className="header">
         <h1 className="title">Red Ping</h1>
-        <nav className="nav">
-          <Link className="Link" to="/home">
-            <h3>Home</h3>
-          </Link>
-          <br></br>
-          <Link className="Link" to="/">
-            <h3>Alert List</h3>
-          </Link>
-          <br></br>
-          <Link className="Link" to="/profile">
-            <h3>Profile</h3>
-          </Link>
-        </nav>
       </header>
       <section className="main">
         <Outlet />
