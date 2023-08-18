4c03356b27b3f3feab5a76cb38f92edfeaf3026e
Min <min.elliot48@gmail.com>
2023-08-18 14:09:33 +1200

    home page buttons added and functional

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -5,10 +5,12 @@ import { useQuery } from '@tanstack/react-query'
 import { getUserFeed } from '../apis/userFeed'
 import { useEffect, useState } from 'react'
 import uploader from './ProfilePage'
+import { MDBNavbarLink } from 'mdb-react-ui-kit'
 
 export default function HomePage() {
   const [timeOfDay, setTimeOfDay] = useState('waiting')
   const [greeting, setGreeting] = useState('Hello!')
+  const [buttonColor, setButtonColor] = useState('primary')
   useEffect(() => {
     const now = new Date()
     const hours = now.getHours()
@@ -17,12 +19,15 @@ export default function HomePage() {
     if (hours < 12) {
       setGreeting('Good Morning! 🐔')
       setTimeOfDay('dawn')
+      setButtonColor('success')
     } else if (hours < 18) {
       setGreeting('Good Afternoon 🐈')
       setTimeOfDay('waiting')
+      setButtonColor('warning')
     } else if (hours >= 18 && hours < 24) {
       setGreeting('Good Evening 🥰')
       setTimeOfDay('dusk')
+      setButtonColor('danger')
     } else {
       setTimeOfDay('waiting')
     }
@@ -37,13 +42,25 @@ export default function HomePage() {
       <br></br>
       <br></br>
       <br></br>
+
+      <div className="button-container">
+        <MDBNavbarLink href="/">
+          <Button variant={buttonColor} size="lg" className="round-button">
+            Alert List
+          </Button>
+        </MDBNavbarLink>
+        <MDBNavbarLink href="/profile">
+          <Button variant={buttonColor} size="lg" className="round-button">
+            Profile
+          </Button>
+        </MDBNavbarLink>
+      </div>
+      <br></br>
       <br></br>
       <br></br>
       <Alert className="alert">
         Welcome to Your Personal Digital Alert App
       </Alert>
-      <br></br>
-      <br></br>
     </div>
   )
 }
